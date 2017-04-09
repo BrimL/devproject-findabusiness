ed6df506f82e7eb4f7a9715e89b1a2675ffa6a00
BrimL <Brimeyer.L@gmail.com>
2017-04-09 12:28:53 -0400

    Add Server Logic for Missing Photo Array
    Major Changes:
    - Add logic to account for missing photo object
    Minor changes:
    - Add reminder to validate fields posted to server

--- dist/findabusiness/routes/default.js
+++ dist/findabusiness/routes/default.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
-
 /* GET home page. */
 router.get('/',(req, res, next)=>{
   res.render('index', { title: 'Find A Business' });
@@ -11,13 +9,21 @@ router.get('/',(req, res, next)=>{
 /*POST home page.*/
 router.post('/',(req, res, next)=>{
   try{
-    //Log client object fields
+    /*Insert object field validation*/
+
+    /*Log client object fields*/
     let place = req.body;
     console.log(place.name);
     console.log(place.formatted_address);
     console.log(place.formatted_phone_number);
     console.log(place.website);
     console.log(place.types);
+
+    /*If there are photos returned, log them as well*/
+    if(place.photos){
+      console.log(place.photos);
+      /*insert calls for image processing*/
+    }
 
     //send response object;
     res.send(place);
