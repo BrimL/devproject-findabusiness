d6c219d1586b800f8bd0b528e383c56d9e26a058
BrimL <Brimeyer.L@gmail.com>
2017-04-09 15:27:04 -0400

    Update Backend Processing
    Major Changes:
    - Add Promises to proivde a response to the client
    - Add Clarifai service call to return tags of image
    - Add function to process POST fields in future
    Minor Change:
    - Provide placeholder for img while working out the issue with Google
    Places no providing 'photo_reference'

--- dist/findabusiness/routes/default.js
+++ dist/findabusiness/routes/default.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var Clarifai = require('clarifai');
+
+var app = new Clarifai.App(
+  'NO18sIhXk9nZDkAdVXNPSThzPXPI8wHn78vAncxe',
+  'c2vHENnTnNj6XdFkXCEWbG1g1oSdBmTqOTO44eP9'
+);
 
 /* GET home page. */
 router.get('/',(req, res, next)=>{
@@ -10,27 +16,42 @@ router.get('/',(req, res, next)=>{
 router.post('/',(req, res, next)=>{
   try{
     /*Insert object field validation*/
+    let imgURL = 'http://lorempixel.com/100/190/nature/6';
 
-    /*Log client object fields*/
-    let place = req.body;
-    console.log(place.name);
-    console.log(place.formatted_address);
-    console.log(place.formatted_phone_number);
-    console.log(place.website);
-    console.log(place.types);
-
-    /*If there are photos returned, log them as well*/
-    if(place.photos){
-      console.log(place.photos);
-      /*insert calls for image processing*/
-    }
-
-    //send response object;
-    res.send(place);
+    /*Promise to build a response based on valid fields and Clarifai results*/
+    Promise.all([validateFields(req.body),app.models.predict(Clarifai.GENERAL_MODEL, imgURL)]).
+    then(buildResult(values),
+    function(err) {
+      console.log(err);
+    });
   }
   catch(err){
     console.log(err);
   }
 });
+
+/* TODO: Move this to a controller module*/
+function validateFields(place){
+  console.log(place.name);
+  console.log(place.formatted_address);
+  console.log(place.formatted_phone_number);
+  console.log(place.website);
+  console.log(place.types);
+  return place;
+}
+
+function buildResult(values){
+  console.log(values);
+  let place = values[0];
+  let imgData = values[1];
+  let tags = imgData.outputs[0].data.concepts;
+
+  place.photos = {
+    'imgURL':'http://lorempixel.com/100/190/nature/6',
+    'tags':tags
+  };
+  console.log(place);
+  res.send(place);
+}
 
 module.exports = router;
